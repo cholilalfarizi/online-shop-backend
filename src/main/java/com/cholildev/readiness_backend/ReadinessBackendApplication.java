a811853e75df926b78968ae96c1772d61f5b59b4
Cholil Alfarizi <cholil.alfarizi@gmail.com>
2024-09-11 16:58:15 +0700

    Initial commit

---
+++ src/main/java/com/cholildev/readiness_backend/ReadinessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cholildev.readiness_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadinessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cholildev/readiness_backend/ReadinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cholildev.readiness_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
